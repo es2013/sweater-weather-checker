c31fba88acbc4c7bb57e792aef49f4328ed05f07
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-01-26 16:10:17 -0800

    create the title based on city that was selected.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ let cityInput = document.querySelector("#city-input");
 let cityText = document.querySelector(".city-text");
 let searchButton = document.querySelector("button");
 
+
+
 //get value from localStorage
 let storedInput = localStorage.getItem('cityInput');
 
@@ -19,6 +21,7 @@ cityInput.addEventListener("input",function(city){
 
 //create a function that saves the city everytime i click on button
 let displayCity = function(){
+    // let cityList = JSON.parse(localStorage.getItem('cityInput')) || [];
     localStorage.setItem('cityInput', cityText.textContent)
 }
 
@@ -34,15 +37,27 @@ function getWeather() {
     // Use `.value` to capture the value of the input and store it in the variable
     var searchCity = document.querySelector('#city-input').value;
     fetch(
-        `http://api.openweathermap.org/data/2.5/weather?&appid=d1dfd9b71c61f4f9b6151a02ee936efa&q=${searchCity}`
+        `http://api.openweathermap.org/data/2.5/weather?&appid=d1dfd9b71c61f4f9b6151a02ee936efa&q=${searchCity}`+'&units=imperial'
         
     )
         .then(function (response) {
             return response.json();
         })
-        .then(function (response) {
-            console.log(response);
-            console.log(searchCity);
+        .then(function (data) {
+            // console.log(data);
+            // console.log(searchCity);
+
+            let currentWeather = document.querySelector("#current-city");
+            let cityTitle = document.createElement('h3');
+
+            
+            //get name of city input into current weather section
+            cityTitle.textContent = data.name;
+            currentWeather.appendChild(cityTitle);
+            
+            
+
+
 
 
         });
